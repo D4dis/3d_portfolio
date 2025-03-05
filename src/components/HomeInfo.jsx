89002284961544bcc415c7c33b4df46de88b8160
Dadis <raditztv@gmail.com>
2025-03-05 22:09:58 +0100

    popup fixed

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { arrow } from '../assets/icons';
 
-const InfoBox = ({ text, link, btnText }) => {
+const InfoBox = ({ text, link, btnText }) => (
   <div className='info-box'>
     <p className='font-medium sm:text-xl text-center'>{text}</p>
     <Link to={link} className='neo-brutalism-white neo-btn'>
@@ -10,7 +10,7 @@ const InfoBox = ({ text, link, btnText }) => {
       <img src={arrow} alt="arrow" className='w-4 h-4 object-contain' />
     </Link>
   </div>
-}
+)
 const renderContent = {
   1: (
     <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
@@ -27,10 +27,18 @@ const renderContent = {
     />
   ),
   3: (
-    <h1>3</h1>
+    <InfoBox
+      text="Led multiple projects to success over the years. Curious about the impact?"
+      link="/projects"
+      btnText="Visit my portfolio"
+    />
   ),
   4: (
-    <h1>4</h1>
+    <InfoBox
+      text="Need a project done or looking for a dev? I'm just a few keystrokes away"
+      link="/contact"
+      btnText="Let's talk"
+    />
   ),
 }
 
